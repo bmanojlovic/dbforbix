107668b8f9b72f3405f4b6fac4a9998a47a4e1ef
rootzone <rootzone@32f9ae8e-14f7-4c8c-b606-976a8fecd463>
2011-05-06 16:37:33 +0000

    git-svn-id: https://dbforbix.svn.sourceforge.net/svnroot/dbforbix@2 32f9ae8e-14f7-4c8c-b606-976a8fecd463

---
+++ 0.2/com/smartmarmot/dbforbix/Querybox.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2010 Andrea Dalle Vacche.
+ * 
+ * This file is part of DBforBIX.
+ *
+ * DBforBIX is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * DBforBIX is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * DBforBIX. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.smartmarmot.dbforbix;
+
+import org.apache.log4j.Level;
+
+public class Querybox {
+	private Query[] _query;
+	private String _dbname;
+	private String _queriesfile;
+
+	public Querybox(String _localdbname, String _prp) {
+		try {
+			setQueries(Configurator.getQueries(Configurator
+					.getPropsFromFile(_prp)));
+			this.setQueriesFile(_prp);
+
+			if (_localdbname.length() > 0 && _localdbname != null) {
+				this.setDBName(_localdbname);
+			}
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			Configurator.logThis(Level.ERROR, "Error on QueryBox "
+					+ e.getMessage());
+		}
+
+	}
+
+	public String getDBName() {
+		return _dbname;
+	}
+
+	public Query[] getQueries() {
+		return _query;
+	}
+
+	public String getQueriesFile() {
+		return _queriesfile;
+	}
+
+	public void refresh() {
+		Query queryTmp[] = this.getQueries();
+		this.setQueries(Configurator.refresh(queryTmp, _queriesfile));
+	}
+
+	public void setDBName(String _dbname) {
+		this._dbname = _dbname;
+	}
+
+	public void setQueries(Query[] _query) {
+		this._query = _query;
+	}
+
+	public void setQueriesFile(String _queriesfile) {
+		this._queriesfile = _queriesfile;
+	}
+
+}
